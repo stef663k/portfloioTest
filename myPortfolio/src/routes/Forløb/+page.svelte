af924170744c7727dc31b06b945c2161ded4438f
stef663k <stef663k@elev.techcollege.dk>
2023-10-05 10:11:23 +0200

    almost comleted missing date changes

--- myPortfolio/src/routes/Forløb/+page.svelte
+++ myPortfolio/src/routes/Forløb/+page.svelte
@@ -1,32 +1,34 @@
 <script>
+    import { text } from "@sveltejs/kit";
+    import { onMount } from "svelte";
+
      
     const currentDate = new Date();
 
        /**
-     * @type {{ [key: string]: Date }}
+     * @type {{ [key: string]: { date: Date, name: string } }}
      */
     const dates = {
-        G2: new Date('2021-01-18'),
-        P1: new Date('2021-09-15'),
-        H1: new Date('2022-02-28'),
-        P2: new Date('2022-05-12'),
-        H2: new Date('2022-08-01'),
-        P3: new Date('2022-10-08'),
-        H3: new Date('2023-10-09'),
-        P4: new Date('2023-12-16'),
-        H4: new Date('2024-10-07'),
-        P5: new Date('2024-10-16'),
-        H5: new Date('2025-01-29'),
-        P6: new Date('2025-04-09'),
-        H6: new Date('2025-10-20'),
-        p7: new Date('2025-11-22'),
-        Slut: new Date('2025-12-31')
+        G2: { date: new Date('2021-01-18'), name: "Grundforløb 2" },
+        P1: { date: new Date('2021-09-15'), name: "Praktikforløb 1" },
+        H1: { date: new Date('2022-02-28'), name: "Hovedforløb 1" },
+        P2: {date: new Date('2022-05-12'), name: "Praktikforløb 2"},
+        H2: {date: new Date('2022-08-01'), name: "Hovedforløb 2"},
+        P3: {date: new Date('2022-10-08'), name: "Praktikforløb 3"},
+        H3: {date: new Date('2023-10-09'), name: "Hovedforløb 3"},
+        P4: {date: new Date('2023-12-16'), name: "Praktikforløb 4"},
+        H4: {date: new Date('2024-10-07'), name: "Hovedforløb 4"},
+        P5: {date: new Date('2024-10-16'), name: "Praktikforløb 5"},
+        H5: {date: new Date('2025-01-29'), name: "Hovedforløb 5"},
+        P6: {date: new Date('2025-04-09'), name: "Praktikforløb 6"},
+        H6: {date: new Date('2025-10-20'), name: "Hovedforløb 6"},
+        p7: {date: new Date('2025-11-22'), name: "Praktikforløb 7"},
+        Slut: {date: new Date('2025-12-31'), name: "Slut"}
     };
 
     /**
-     * Format a date as a string.
-     * @param {Date} date - The date to format.
-     * @returns {string} The formatted date string.
+     * @param {Date} date 
+     * @returns {string} 
      */
     function formatDate(date) {
         const year = date.getFullYear();
@@ -36,22 +38,23 @@
         return `${year} ${month} ${day}`;
     }
 
-    for (const key in dates) {
-        if (dates.hasOwnProperty(key)) {
-            if (currentDate >= dates[key]) {
+    onMount(() => {
+        for (const key in dates) {
+            if (dates.hasOwnProperty(key)) {
+                const course = dates[key];
                 const element = document.getElementById(`date${key}`);
                 if (element) {
-                    element.textContent = `(${formatDate(dates[key])}) - Completed`;
-                }
-            } else {
-                const element = document.getElementById(`date${key}`);
-                if (element) {
-                    element.textContent = `(${formatDate(dates[key])}) - Upcoming`;
+                    if (currentDate >= course.date) {
+                        element.textContent = course.name;
+                        element.classList.add("completed");
+                    } else {
+                        element.textContent = course.name;
+                        element.classList.add("upcoming");
+                    }
                 }
             }
         }
-    }
-
+    });
 
 </script>
 <header>
@@ -66,14 +69,27 @@
             align-items: center;
             text-align: center;
             flex-direction: column;
-            border: 1px solid;
             padding: 10px
         }
         span{
             padding: 10px;
             width: 50%;
             border: 1px solid;
+            color: white;
+            background-color: #998d7b;
         }
+        span:hover{
+            background-color: #807667;
+        }
+        .completed{
+            background-color: darkgrey;
+            color: black;
+            text-decoration: line-through;
+        }
+        .completed:hover{
+            background-color: grey;
+        }
+
     </style>
     <title>Forløb</title>
 </header>
@@ -82,19 +98,11 @@
     <h1>Forløb</h1>
 </div>
 <div class="dates">
-    <span class="grundforløb" id="dateG2">Grundforløb 2</span>
-    <span class="p1" id="dateP1">Praktikforløb 1</span>
-    <span class="h1" id="dateH1">Hovedforløb 1</span>
-    <span class="p2" id="dateP2">praktikforløb 2</span>
-    <span class="h2" id="dateH2">Hovedforløb 2</span>
-    <span class="p3" id="dateP3">Praktikforløb 3</span>
-    <span class="h3" id="dateH3"> Hovedforløb 3</span>
-    <span class="p4" id="dateP4">Praktikforløb 4</span>
-    <span class="h4" id="dateH4"> Hovedforløb 4</span>
-    <span class="p5" id="dateP5"> Praktikforløb 5</span>
-    <span class="h5" id="dateH5">Hovedforløb 5</span>
-    <span class="p6" id="dateP6">Praktikforløb 6</span>
-    <span class="h6" id="dateH6">Hovedforløb 6</span>
-    <span class="p7" id="dateP7">Praktikforløb 7</span>
-    <span class="slut" id="dateSlut">SLUT</span>
+    <br/>
+    {#each Object.keys(dates) as key}
+        <span id={'date${key}'} class={key} >
+            ({formatDate(dates[key].date)}) - {dates[key].name}
+        </span>
+    {/each}
+    <br/>
 </div>
