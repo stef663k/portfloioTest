72d4dec9234522b08512b99cb60b92ba3ac90b6d
stef663k <stef663k@elev.techcollege.dk>
2023-10-05 13:07:28 +0200

    vite

--- myPortfolio/vite.config.js
+++ myPortfolio/vite.config.js
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import ts from 'rollup-plugin-typescript2';
 
+ 
 
 export default defineConfig({
-	plugins: [sveltekit(),
-	ts({ tsconfig: './.svelte-kit/tsconfig.json' }),]
+	plugins: [sveltekit()]
 });
